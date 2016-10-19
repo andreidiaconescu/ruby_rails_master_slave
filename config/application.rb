40cc9f9b546ec87b955f5c37752167620a152830
andrei.diaconescu <andrei.diaconescu@anais.digital>
2016-10-19 12:01:23 +0300

    try again to disable database ActiveRecord Query cache, to test master/slave

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,3 @@ module Blog
         # -- all .rb files in that directory are automatically loaded.
     end
 end
-
-config.middleware.delete "ActiveRecord::QueryCache"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,6 @@ Rails.application.configure do
     # routes, locales, etc. This feature depends on the listen gem.
     config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+    config.middleware.delete "ActiveRecord::QueryCache"
 
 end
